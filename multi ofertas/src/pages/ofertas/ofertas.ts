e22657cb550beff2ca82b52c6cb0cde4262a850a
Carlos Henrique <carlos_hpc@hotmail.com>
2017-09-12 16:15:04 -0300

    pesquisa e reverse adicionado

--- multi ofertas/src/pages/ofertas/ofertas.ts
+++ multi ofertas/src/pages/ofertas/ofertas.ts
@@ -21,13 +21,17 @@ import {OfertasService} from '../../providers/ofertas-service';
 export class Ofertas {
 
   ofertas: any;
+  public ofertas_original: any;
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public ofertasService: OfertasService,
     public loadingCtrl: LoadingController,) {
     this.presentLoading();
+
   }
+
 
   presentLoading() {
    let loader = this.loadingCtrl.create({
@@ -42,11 +46,40 @@ export class Ofertas {
     this.ofertasService.getOfertas()
       .then(data => {
         this.ofertas = data;
+        // cria uma lista com o valor original
+        this.ofertas_original = this.ofertas;
         loader.dismiss();
         console.log(this.ofertas);
 
       });
   }
+
+  //busca na lista os itens
+  getItems(ev: any) {
+
+    // carrega o valor original da lista
+     this.ofertas = this.ofertas_original;
+
+     // set val to the value of the searchbar
+     let val = ev.target.value;
+
+     console.log('ev.target.value', val);
+
+     // if the value is an empty string don't filter the items
+     if (val && val.trim() != '') {
+       this.ofertas = this.ofertas.filter((oferta) => {
+         return (oferta.produto.toLowerCase().indexOf(val.toLowerCase()) > -1);
+       })
+     }
+   }
+
+   // criar um filtro para reodenar a lista
+   //https://www.joshmorony.com/filtering-mapping-and-reducing-arrays-in-ionic-2/
+   reverse(){
+     this.ofertas = this.ofertas.reverse();
+
+   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Ofertas');
